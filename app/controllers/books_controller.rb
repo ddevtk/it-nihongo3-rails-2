f8c74c34b6b3a2cbdc74f349c1da2188ec5f88cc
duongbui123 <buiducduong2342000@gmail.com>
2021-12-13 13:46:34 +0700

    add details book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,24 +5,19 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
-
   # GET /books/1 or /books/1.json
   def show
   end
-
   # GET /books/new
   def new
     @book = Book.new
   end
-
   # GET /books/1/edit
   def edit
   end
-
   # POST /books or /books.json
   def create
     @book = Book.new(book_params)
-
     respond_to do |format|
       if @book.save
         format.html { redirect_to @book, notice: "Book was successfully created." }
@@ -33,7 +28,6 @@ class BooksController < ApplicationController
       end
     end
   end
-
   # PATCH/PUT /books/1 or /books/1.json
   def update
     respond_to do |format|
@@ -46,7 +40,6 @@ class BooksController < ApplicationController
       end
     end
   end
-
   # DELETE /books/1 or /books/1.json
   def destroy
     @book.destroy
@@ -55,7 +48,6 @@ class BooksController < ApplicationController
       format.json { head :no_content }
     end
   end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_book
@@ -64,6 +56,6 @@ class BooksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def book_params
-      params.require(:book).permit(:title, :number_of_pages, :image)
+      params.require(:book).permit(:title, :number_of_pages, :image, :detail)
     end
 end
